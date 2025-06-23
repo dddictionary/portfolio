5a3b1975d244b14803e220f63bb5f1658faee8b3
Aaron <1kookyw1@gmail.com>
2025-06-22 17:04:35 -0700

    add work template and styling

--- app/__init__.py
+++ app/__init__.py
@@ -19,14 +19,65 @@ def nav_items():
 @app.context_processor
 def hobby_items():
     hobbyitems = [
-        {'title': 'Roblox', 'description': 'I love playing roblox it is so fun and I do this all day', 'source': 'https://i.pinimg.com/originals/37/07/a7/3707a7cd7d384511c213b2a12dc3f0a7.jpg'},
-        {'title': 'Mountain Climbing', 'description': 'There is nothing like climbing up a cliff-side on a hot sunny day with my VR', 'source': 'https://i.ytimg.com/vi/xAYuh4NQVeE/maxresdefault.jpg'},
-        {'title': 'Lorem', 'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.', 'source': 'https://dummyimage.com/500x500/ffffff/000000'},
-        {'title': 'Lorem', 'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.', 'source': 'https://dummyimage.com/500x500/ffffff/000000'},
-        {'title': 'Lorem', 'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.', 'source': 'https://dummyimage.com/500x500/ffffff/000000'},
-        {'title': 'Lorem', 'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.', 'source': 'https://dummyimage.com/500x500/ffffff/000000'},
+        {
+            'title': 'Roblox',
+            'description': 'I love playing roblox it is so fun and I do this all day',
+            'source': 'https://i.pinimg.com/originals/37/07/a7/3707a7cd7d384511c213b2a12dc3f0a7.jpg'
+        },
+        {
+            'title': 'Mountain Climbing', 
+            'description': 'There is nothing like climbing up a cliff-side on a hot sunny day with my VR', 
+            'source': 'https://i.ytimg.com/vi/xAYuh4NQVeE/maxresdefault.jpg'
+        },
+        {
+            'title': 'Lorem', 
+            'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.', 
+            'source': 'https://dummyimage.com/500x500/ffffff/000000'
+        },
+        {
+            'title': 'Lorem', 
+            'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.', 
+            'source': 'https://dummyimage.com/500x500/ffffff/000000'
+        },
+        {
+            'title': 'Lorem', 
+            'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.', 
+            'source': 'https://dummyimage.com/500x500/ffffff/000000'
+        },
+        {
+            'title': 'Lorem', 
+            'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.', 
+            'source': 'https://dummyimage.com/500x500/ffffff/000000'
+        },
     ]
     return {'hobbies': hobbyitems}
+
+@app.context_processor
+def work_experiences():
+    work_data = [
+        {
+            'title': 'Apple',
+            'role': 'Full-stack Developer',
+            'startdate': 'Jan 2023',
+            'enddate': 'Present',
+            'description': 'Ate apples all day and removed oranges from the restrooms every night. Blockchain bananas.'
+        },
+        {
+            'title': 'Google',
+            'role': 'Front-end Specialist',
+            'startdate': 'Jul 2021',
+            'enddate': 'Dec 2022',
+            'description': 'Used Google search 10000 times to search up how to create a website'
+        },
+        {
+            'title': 'Meta',
+            'role': 'Mobile App Intern',
+            'startdate': 'May 2021',
+            'enddate': 'Aug 2021',
+            'description': 'Assisted in the development of a mobile application for inventory management. Probably gained experience with mobile development frameworks and agile methodologies.'
+        }
+    ]
+    return {'work': work_data}
 
 @app.route('/')
 def index():
