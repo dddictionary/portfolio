dbe47be9d4f6c314b88cf2f376e1869cc732c3cf
aaron <awu0103@gmail.com>
2025-07-11 07:18:57 +0000

    rewrite redeploy.sh and test change

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
-session_name="flask_app"
-venv_path="python3-virtualenv/bin/activate"
-tmux kill-server || true
 git fetch origin
 git reset --hard origin/main
-tmux new-session -d -s ${session_name}
-tmux send-keys -t ${session_name} "source ${venv_path}" C-m
-tmux send-keys -t ${session_name} "pip install -r requirements.txt" C-m
-tmux send-keys -t ${session_name} "flask run --host=0.0.0.0" C-m
+systemctl daemon-reload
+systemctl restart myportfolio
