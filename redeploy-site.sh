e7fbff9e725ae34351221b9dfd72f8973b1a1c6b
Abrar Habib <abrarhabib285@gmail.com>
2025-07-14 12:19:54 -0400

    feat: flask app is now a systemd service

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -7,25 +7,19 @@ PROJECT_DIR="/root/portfolio"
 echo "Changing into portfolio directory"
 cd "${PROJECT_DIR}"
 
-# kill all tmux sessions
-echo "Killing all existing tmux sessions"
-tmux kill-server || true
-
-
-# run git commands
 echo "Fetching latest changes from GitHub"
 git fetch && git reset origin/main --hard
 
-# enter the python virtual environment and install python dependencies
 echo "Activating virtual environment"
 source .venv/bin/activate
 
 echo "Installing packages"
 pip install -r requirements.txt
 
-# start a new detached tmux sessions
-echo "Starting new tmux session"
-tmux new-session -d -s portfolio
+echo "Restarting service"
+sudo systemctl restart myportfolio
 
-echo "Running Flask app inside tmux session"
-tmux send-keys -t portfolio "flask run --host=0.0.0.0" C-m
+echo "Checking service status"
+sudo systemctl status myportfolio --no-pager
+
+echo "Deployment successful!"
