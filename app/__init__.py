f01f05f0f20b5192c33775a695eb9b1efed7fb15
Abrar Habib <abrarhabib285@gmail.com>
2025-07-14 11:17:37 -0400

    fix: changed env name

--- app/__init__.py
+++ app/__init__.py
@@ -13,7 +13,7 @@ mydb = MySQLDatabase(
     os.getenv("MYSQL_DATABASE"),
     user=os.getenv("MYSQL_USER"),
     password=os.getenv("MYSQL_PASSWORD"),
-    host=os.getenv("MYSELF_HOST"),
+    host=os.getenv("MYSQL_HOST"),
     port=3306,
 )
 
