ca8717ace7dd1235ac335016cdf393e9b7df9a61
Abrar Habib <abrarhabib285@gmail.com>
2025-07-14 11:25:16 -0400

    feat: updated redploy-site.sh

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,23 +1,29 @@
 #!/bin/bash
 
+set -e
+
+PROJECT_DIR="/root/portfolio"
+
+echo "Changing into portfolio directory"
+cd "${PROJECT_DIR}"
+
 # kill all tmux sessions
+echo "Killing all existing tmux sessions"
 tmux kill-server
 
-# cd into project directory
-
-cd mlh-week1
 
 # run git commands
+echo "Fetching latest changes from GitHub"
 git fetch && git reset origin/main --hard
 
 # enter the python virtual environment and install python dependencies
-
+echo "Activating virtual environment"
 source .venv/bin/activate
+
+echo "Installing packages"
 pip install -r requirements.txt
 
-# start a new detached tmux session
-cd ..
-deactivate
-
+# start a new detached tmux sessions
+echo "Starting new tmux session"
 tmux new-session -d -s portfolio
-tmux send-keys -t portfolio "cd mlh-week1 && source .venv/bin/activate && flask run --host=0.0.0.0" C-m
+tmux send-keys -t portfolio "flask run --host=0.0.0.0" C-m
