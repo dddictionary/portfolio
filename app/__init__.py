b1f825378a1d22fb94451f85977110f90fcc7529
Abrar Habib <abrarhabib285@gmail.com>
2025-07-18 16:37:01 -0400

    feat: added more tests for flask api

--- app/__init__.py
+++ app/__init__.py
@@ -14,7 +14,6 @@ if os.getenv("TESTING") == "true":
     print("Running in test mode")
     mydb = SqliteDatabase("file:memory?mode=memory&cache=shared", uri=True)
 else:
-
     mydb = MySQLDatabase(
         os.getenv("MYSQL_DATABASE"),
         user=os.getenv("MYSQL_USER"),
@@ -227,9 +226,13 @@ def travel_experiences():
 # def education():
 #     return render_template('education.html', title="MLH Fellow - Education", url=os.getenv("URL"))
 
-#@app.route('/timeline')
-#def timeline():
-#    return render_template('timeline.html', title="MLH Fellow - Timeline", url=os.getenv("URL"))
+
+@app.route("/timeline")
+def timeline():
+    return render_template(
+        "timeline.html", title="MLH Fellow - Timeline", url=os.getenv("URL")
+    )
+
 
 @app.route("/travels")
 def travels():
@@ -250,15 +253,18 @@ def get_time_line_post():
             for p in TimelinePost.select().order_by(TimelinePost.created_at.desc())
         ]
     }
-@app.route('/api/timeline_post', methods=['POST'])
+
+
+@app.route("/api/timeline_post", methods=["POST"])
 def post_time_line_post():
-    name = request.form['name']
-    email = request.form['email']
-    content = request.form['content']
+    name = request.form["name"]
+    email = request.form["email"]
+    content = request.form["content"]
     timeline_post = TimelinePost.create(name=name, email=email, content=content)
     return model_to_dict(timeline_post)
 
-@app.route('/api/delete_timeline_post/<int:post_id>', methods=['DELETE'])
+
+@app.route("/api/delete_timeline_post/<int:post_id>", methods=["DELETE"])
 def delete_time_line_post(post_id):
     try:
         with mydb.connection_context():
--- tests/test_app.py
+++ tests/test_app.py
@@ -15,7 +15,7 @@ class AppTestCase(unittest.TestCase):
         print(f"Home page status code: {response.status_code}")
         assert response.status_code == 200
         html = response.get_data(as_text=True)
-        assert "<title>MLH Fellow</title>" in html
+        assert "airwu.dev" in html
         # TODO: Add more tests relating to the home page
 
     def test_timeline(self):
@@ -25,3 +25,8 @@ class AppTestCase(unittest.TestCase):
         json = response.get_json()
         assert "timeline_posts" in json
         assert len(json["timeline_posts"]) == 0
+
+        # use the /api/timeline_post endpoint to push a new post then use the get to verify that the len went up by 1
+        # TODO: Add more tests relating to the /api/timeline_post GET and POST apis
+        # get the html of the timeline page and assert certain things exist in there
+        # TODO: Add more tests relating to the timeline page
