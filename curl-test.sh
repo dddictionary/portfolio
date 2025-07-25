0354a2ea6829f92ae6b5f04fd5abe5612dfcf720
aaron <awu0103@gmail.com>
2025-07-25 09:09:20 +0000

    delete: remove old scripts from root

--- curl-test.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-curl --request POST http://localhost:5000/api/timeline_post -d 'name=Aaron&email=a@example.io&content=first test post'
-response=$(curl http://localhost:5000/api/timeline_post)
-echo $response
-exit 0
--- redeploy-dev.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-#git fetch origin
-#git reset --hard origin/main
-systemctl daemon-reload
-systemctl restart mysqld
-systemctl restart myportfolio
